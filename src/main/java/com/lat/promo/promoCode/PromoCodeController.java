1f88ed8ade9876f907455c56ac567922f17d449d
Artur <artur.pietrzak1@outlook.com>
2024-05-06 22:29:58 +0200

    Create initial version of Controller-Service-Repository pattern for PromoCode

--- src/main/java/com/lat/promo/promoCode/PromoCodeController.java
+++ src/main/java/com/lat/promo/promoCode/PromoCodeController.java
@@ -1,2 +1,32 @@
-package com.lat.promo.promoCode;public class PromoCodeController {
+package com.lat.promo.promoCode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/promo-codes")
+public class PromoCodeController {
+    private final PromoCodeService promoCodeService;
+
+    @Autowired
+    public PromoCodeController(PromoCodeService promoCodeService) {
+        this.promoCodeService = promoCodeService;
+    }
+
+    @GetMapping
+    public List<PromoCode> getPromoCodes(){
+        return promoCodeService.getAllPromoCodes();
+    }
+
+    @GetMapping("/{promoCode}")
+    public PromoCode getPromoCodeByCode(@PathVariable String promoCode){
+        return promoCodeService.getPromoCodeByCode(promoCode);
+    }
+
+    @PostMapping
+    public void addPromoCode(@RequestBody PromoCode promoCode) {
+        promoCodeService.addNewPromoCode(promoCode);
+    }
 }
--- src/main/java/com/lat/promo/promoCode/PromoCodeService.java
+++ src/main/java/com/lat/promo/promoCode/PromoCodeService.java
@@ -1,2 +1,35 @@
-package com.lat.promo.promoCode;public class PromoCodeService {
+package com.lat.promo.promoCode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PromoCodeService {
+    @Autowired
+    PromoCodeRepository promoCodeRepository;
+
+    public List<PromoCode> getAllPromoCodes() {
+        return promoCodeRepository.findAll();
+    }
+
+    public void addNewPromoCode(PromoCode promoCode) {
+        PromoCode promoCodeObject = promoCodeRepository.save(promoCode);
+    }
+
+    public PromoCode getPromoCodeByCode(String code) {
+        Optional<PromoCode> promoCodeObject = promoCodeRepository.findPromoCodeByCode(code);
+
+        if (promoCodeObject.isPresent()) {
+            return promoCodeObject.get();
+        } else {
+            return null;
+        }
+    }
 }
